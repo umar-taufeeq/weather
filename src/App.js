388458142395e57d870c0301f2cb3d07f3029a5f
umar-taufeeq <umarckp111@gmail.com>
2025-01-26 19:00:43 +0530

    Add project files including React setup, styles, and testing configuration

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+       <div className="footer-info"> 
+        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93"> 
+         </a>{" "} 
+         Developed by{" "} 
+         <a target="_blank" href="https://www.gauravghaiw.dev/"> 
+           umar taufee ahmed
+         </a>{" "} 
+        {" "} 
+        <a target="_blank" href="https://www.htmlhints.com/"> 
+           
+         </a> 
+       </div> 
+    </React.Fragment>
+  );
+}
+
+export default App;
